4bd2c037bc7ed0e43e292295e258a98bba8991df
peychma <mariapei4eva@gmail.com>
2023-11-04 16:07:32 +0200

    .

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,17 @@
-function getElementWidth(content, padding, border) {
-return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+function checkForSpam(message) {
+    let lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
